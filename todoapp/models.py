a2da7c02463ce0a61dfd74e8eb2b4d12a6e2328a
Michał Kowieski <kowieski.m98@gmail.com>
2019-08-07 23:58:32 +0200

    Add two models Task and Hashtag.

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Hashtag(models.Model):
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(null=False)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    hashtags = models.ManyToManyField(Hashtag)
