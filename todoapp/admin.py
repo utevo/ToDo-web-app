aadaad10752de176e3d2e25e8c82190d367f1e09
Michał Kowieski <kowieski.m98@gmail.com>
2019-08-08 22:01:57 +0200

    Make few change in models. Update .gitignore. Update urls.py.

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from todoapp.models import Task, Hashtag 
+
 # Register your models here.
+
+
+admin.site.register(Task)
+
+admin.site.register(Hashtag)
--- todoapp/models.py
+++ todoapp/models.py
@@ -5,14 +5,25 @@ from django.utils import timezone
 # Create your models here.
 
 class Hashtag(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=30, unique=True)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
 
 
 class Task(models.Model):
     title = models.CharField(max_length=30)
-    description = models.TextField(null=False)
+    description = models.TextField(null=False, blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
 
-    hashtags = models.ManyToManyField(Hashtag)
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["completed", "-created_at"]
+        verbose_name_plural = "tasks"
+
+    hashtags = models.ManyToManyField(Hashtag, blank=True)
