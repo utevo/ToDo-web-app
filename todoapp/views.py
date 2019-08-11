e2318b6b3d662e4d87d6b4ca77486a6945ca4461
Michał Kowieski <kowieski.m98@gmail.com>
2019-08-11 22:02:34 +0200

    Create view todoapp:index

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from .models import Task, Hashtag
+
 # Create your views here.
+
+
+def index(request):
+    task_list = Task.objects.all()
+    context = {'task_list': task_list}
+    return render(request, 'todoapp/index.html', context)
+
+
+def task_detail(request, task_id):
+    pass
+
+
+def hashtag_detail(request, hashtag_id):
+    pass
