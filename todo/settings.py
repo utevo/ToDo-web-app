904500021709b995ad8bd99267f969163972a048
Michał Kowieski <kowieski.m98@gmail.com>
2019-08-16 00:01:34 +0200

    Add . to templates dirs (repair last commit)

--- todo/settings.py
+++ todo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todoapp.apps.TodoappConfig',
+    'todoapp',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['.'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
