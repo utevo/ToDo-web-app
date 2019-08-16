a4ebbd040102ded0561f46578803b835951c5f13
Michał Kowieski <kowieski.m98@gmail.com>
2019-08-16 19:55:48 +0200

    Create HashtagForm.

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
-from .models import Task
+from .models import Task, Hashtag
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description',]
+        fields = ['title', 'description', 'hashtags']
+
+class HashtagForm(forms.ModelForm):
+    class Meta:
+        model = Hashtag
+        fields = ['title',]
