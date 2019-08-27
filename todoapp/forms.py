71396a3383299e52d58e5b1f11fa0548de6f29c5
Michał Kowieski <kowieski.m98@gmail.com>
2019-08-27 22:18:09 +0200

    Now in todoapp:TaskForm only request.user hashtags are shown. (add authentication Part 6 [FINAL])

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import ModelMultipleChoiceField
+from django.forms.widgets import SelectMultiple
 
 from .models import Task, Hashtag
 
@@ -6,26 +8,33 @@ from django.core.exceptions import ValidationError
 
 
 class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'description', 'completed', 'hashtags', ]
-        user : int
+
+    _user : int
+    hashtags : ModelMultipleChoiceField
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.user = user
+        self._user =  user
+        self.fields['hashtags'] = ModelMultipleChoiceField(
+                            queryset=Hashtag.objects.filter(owner=self._user), 
+                            widget=SelectMultiple(),
+                            required=False,
+                            )
 
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'completed', 'hashtags', ]
 
     def clean(self):
         """
         Checks that all hashtags belong to the corect user
         """
         hashtags = self.cleaned_data.get('hashtags')
-        for hashtag in hashtags: 
-            print(hashtag.owner)
-            print(self.user)
-            if hashtag.owner != self.user:
-                raise ValidationError("User don't own one of the hashtags.")
+        
+        for hashtag in hashtags:
+            if hashtag.owner != self._user:
+                raise ValidationError(
+                    "User don't own one of the hashtags.")
 
 
 class HashtagForm(forms.ModelForm):
